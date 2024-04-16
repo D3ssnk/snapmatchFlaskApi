51645483364bdb68cdc3d314599a5bbfe9a6d61e
Desange Nkumu <psydn5@exmail.nottingham.ac.uk>
2024-04-16 17:24:46 +0100

    hello

--- app.py
+++ app.py
@@ -161,8 +161,7 @@ def get_leaderboard_route():
         # Get leaderboard from the database
         leaderboard = get_leaderboard_from_database()
         for i, user in enumerate(leaderboard):
-            print(type(user_id), type(user['UserID']))
-            if user['UserID'] == user_id:
+            if user['UserID'] == str(user_id):
                 index = i + 1
                 userInfo = [user]
                 break
